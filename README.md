15f50a9369318d019579d05352eeac943eaef2f0
jhermsmeier <jhermsmeier@googlemail.com>
2014-09-08 03:07:38 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# TCP Shell
+
+## Install via [npm](https://npmjs.org)
+
+```sh
+$ npm install --global tcp-shell
+```
+
+## Usage
+
+```
+Usage: tcp <port> <host>
+
+Options:
+  --help, -h  Display this usage help
+  --tls, -s   Enable TLS
+  --crlf, -c  Use <CRLF> as EOL
+```
+
+## Example
+
+```sh
+$ tcp --tls --crlf 993 imap.gmail.com
+```
+
+```
+[CONNECTION] 192.168.1.5:58499 -> 74.125.25.108:993
+
+* OK Gimap ready for requests from 118.93.93.244 7mb48335792pbn
+1 LOGOUT
+* BYE Logout Requested 7mb48335792pbn
+1 OK Quoth the raven, nevermore... 7mb48335792pbn
+
+[CONNECTION:CLOSE]
+```
---
+++ bin/tcp.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+var TCP = require( 'net' )
+var TLS = require( 'tls' )
+var REPL = require( 'repl' )
+
+var log = console.log.bind( console )
+
+log( '' )
+
+var options = require( 'optimist' )
+  .usage( 'Usage: tcp <port> <host>' )
+  .boolean( 'help' )
+  .describe( 'help', 'Display this usage help' )
+  .boolean( 'tls' )
+  .describe( 'tls', 'Enable TLS' )
+  .boolean( 'crlf' )
+  .describe( 'crlf', 'Use <CRLF> as EOL' )
+  .alias({
+    help: 'h',
+    tls: 's',
+    crlf: 'c',
+  })
+
+var argv = options.argv
+
+if( argv._.length === 0 || argv.help ) {
+  return options.showHelp()
+}
+
+var transport = argv.tls ? TLS : TCP
+var EOL = argv.crlf ? '\r\n' : '\n'
+var socket = transport.connect({
+  port: argv._.shift(),
+  host: argv._.shift(),
+})
+
+var repl = REPL.start({
+  prompt: '',
+  input: process.stdin,
+  output: process.stdout,
+  ignoreUndefined: true,
+  terminal: false,
+  eval: function eval( cmd, context, filename, callback ) {
+    cmd = cmd.replace( /^\(\s*|\s*\)$/mg, '' ) + EOL
+    socket.write( cmd, function() {
+      callback( null )
+    })
+  }
+})
+
+repl.on( 'exit', function() {
+  socket.end()
+  socket.close()
+})
+
+socket.pipe( process.stdout )
+
+socket.on( 'connect', function() {
+  var remote = this.remoteAddress + ':' + this.remotePort
+  var local = this.address().address + ':' + this.address().port
+  log( '[CONNECTION]', local, '->', remote )
+  log( '' )
+})
+
+socket.on( 'secureConnect', function() {
+  var remote = this.remoteAddress + ':' + this.remotePort
+  var local = this.address().address + ':' + this.address().port
+  log( '[CONNECTION]', local, '->', remote )
+  log( '' )
+})
+
+socket.on( 'timeout', function() {
+  log( '' )
+  log( '[CONNECTION:TIMEOUT]' )
+})
+
+socket.on( 'close', function() {
+  log( '' )
+  log( '[CONNECTION:CLOSE]' )
+  process.exit()
+})
+
+socket.on( 'error', function( error ) {
+  log( '' )
+  console.error( '[CONNECTION:ERROR]', error.message )
+})
